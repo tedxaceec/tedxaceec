9c3f17ff0156c3583bd1bd5f54e13fcde13c9e5c
madhavmadupu <madhav.madupu@gmail.com>
2026-02-25 13:27:06 +0530

    feat: add an interactive timeline component to the landing page and introduce a new about page.

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from './(sections)/HeroSection'
 import { TracingBeam } from '@/components/ui/tracing-beam'
+import { EventTimeline } from './(sections)/Timeline'
 
 const page = () => {
   return (
     <TracingBeam>
       <HeroSection />
+      <EventTimeline />
     </TracingBeam>
   )
 }
